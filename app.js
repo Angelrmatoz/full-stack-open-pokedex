17c84200f5cbde9ab967036c8b33a6cf35f25f25
Angel <angelrmatoz@gmail.com>
2026-01-20 16:00:04 -0400

    Add catch-all route for SPA to serve index.html

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
+
+// Catch-all: para SPA, devuelve index.html en rutas no estáticas
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
